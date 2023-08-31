261940dfb6e931e1eb25a9dc685b95c15f37a2da
Raunak876 <109013965+Raunak876@users.noreply.github.com>
2023-08-31 14:52:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+  }
+  
+  function calculate() {
+    let displayValue = document.getElementById("display").value;
+    try {
+      let result = eval(displayValue);
+      document.getElementById("display").value = result;
+    } catch (error) {
+      document.getElementById("display").value = "Error";
+    }
+  }
+  
+  function clearDisplay() {
+    document.getElementById("display").value = "";
+  }
+   
+  
+  
+  
+  
+  
+  
